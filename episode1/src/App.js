ab9e3077f6c58ba58be0135afda1e0d0351f5f1a
huangyifei <903061211@qq.com>
2023-07-10 09:47:54 +0800

    feat:Ternary Operators,Lists and CSS

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -1,27 +1,19 @@
 import './App.css';
-
+import {User} from './User'
 function App() {
+    // const names = ["Pedro","Jake","Jessica","Mike","Dustin"]
+    const users = [
+        {name: "Pedro", age: 21}, {name: "Jake", age: 25}, {name: "Jessica", age: 45}
+    ]
     return (
         <div className="App">
-            <User name="Pedro" age = {21} email = "pedro@qq.com"/>
-            <User name="huang" age = {25} email = "huang@qq.com"/>
-            <User name="liu" age = {18} email = "liu@qq.com"/>
+            {users.map((user, key) => {
+                return (
+                    <User name={user.name} age={user.age}/>
+                )
+            })}
         </div>
     );
 }
 
-const getName = () => {
-    return "pedro"
-}
-
-const User = (props) => {
-    return (
-        <div>
-            <h1>{props.name}</h1>
-            <h1>{props.age}</h1>
-            <h1>{props.email}</h1>
-        </div>
-    )
-}
-//组件大写
 export default App;
