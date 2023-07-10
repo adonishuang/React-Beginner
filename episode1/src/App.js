d2b2206e7563c96cbf892808085f4d0f45e27dd1
huangyifei <903061211@qq.com>
2023-07-10 18:04:11 +0800

    feat:React Query

--- episode1/src/App.js
+++ episode1/src/App.js
@@ -4,26 +4,30 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import {Profile} from "./pages/Profile";
 import {Contact} from "./pages/Contact";
 import {Navbar} from "./Navbar";
-import {useState, createContext} from "react";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
-export const AppContext = createContext();
 
 function App() {
-    const [username, setUsername] = useState("pedroTech");
-
+    const client = new QueryClient({
+        defaultOptions: {
+            queries: {
+                refetchOnWindowFocus: false
+            }
+        }
+    });
     return (
         <div className="App">
-            <AppContext.Provider value={{username,setUsername}}>
+            <QueryClientProvider client={client}>
                 <Router>
                     <Navbar/>
                     <Routes>
-                        <Route path="/" element={<Home />}/>
+                        <Route path="/" element={<Home/>}/>
                         <Route path="/profile" element={<Profile/>}/>
                         <Route path="/contact" element={<Contact/>}/>
                         <Route path="*" element={<h1>Page not Found</h1>}/>
                     </Routes>
                 </Router>
-            </AppContext.Provider>
+            </QueryClientProvider>
         </div>
     )
 }
--- episode1/src/pages/Home.js
+++ episode1/src/pages/Home.js
@@ -1,6 +1,18 @@
-import {useContext} from "react";
-import {AppContext} from "../App"
+import {useQuery} from "@tanstack/react-query";
+import Axios from "axios";
+
 export const Home = () => {
-    const {username } = useContext(AppContext)
-    return <h1>This is HomePage :{username}</h1>
+    const {data,isLoading,isError,refetch} = useQuery(["cat"], () => {
+        return Axios.get("https://catfact.ninja/fact").then((res) => {
+            return res.data;
+        })
+    })
+    if(isLoading){
+        return <h1>IsLoading.....</h1>
+    }
+    if(isError){
+        return <h1>Error</h1>
+    }
+    return <h1>This is HomePage <p>{data?.fact}</p>
+    <button onClick={refetch}>Update Data</button></h1>
 }
